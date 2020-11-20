f836a8a2f2142c37c637e617b100971140082538
tico <58782736+ticod@users.noreply.github.com>
2020-11-20 18:07:57 +0900

    Update: DbConnection public으로 변경

--- src/model/DbConnection.java
+++ src/model/DbConnection.java
@@ -6,12 +6,12 @@ public class DbConnection {
 
     private DbConnection() {}
 
-    static Connection getConnection() {
+    public static Connection getConnection() {
         Connection conn = null;
         try {
             Class.forName("org.mariadb.jdbc.Driver");
             conn = DriverManager.getConnection(
-                    "jdbc:mariadb://localhost:3306/classdb", "scott", "1234"
+                    "jdbc:mariadb://localhost:3307/classdb", "scott", "1234"
             );
             return conn;
         } catch (Exception e) {
@@ -20,7 +20,7 @@ public class DbConnection {
         return conn;
     }
 
-    static void close(Connection conn, Statement stmt, ResultSet rs) {
+    public static void close(Connection conn, Statement stmt, ResultSet rs) {
         try {
             if(rs != null) rs.close();
             if(stmt != null) stmt.close();
